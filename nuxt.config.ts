6efddc37a0bc4b2a421ad451e20b9e9a72b40d17
mahima98 <mahimaramgolam98@gmail.com>
2024-07-01 14:53:00 +0400

    installed nuxt/image package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   extends: "content-wind",
-  modules: ["@nuxthq/studio"],
+  modules: ["@nuxthq/studio", "@nuxt/image"],
   routeRules: {
     "/": { prerender: true },
   },
