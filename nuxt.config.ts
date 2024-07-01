9d94594813e8232177df5a693b00ff0f972a5e46
mahima98 <mahimaramgolam98@gmail.com>
2024-07-01 14:53:46 +0400

    installed nuxt/ui module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   extends: "content-wind",
-  modules: ["@nuxthq/studio", "@nuxt/image"],
+  modules: ["@nuxthq/studio", "@nuxt/image", "@nuxt/ui"],
   routeRules: {
     "/": { prerender: true },
   },
